f6b314f05775ed7543cb2e6f6090a983a95ca9fb
Hanghang Zhang <zhangh40@host-63451.dhcp.egr.msu.edu>
2015-03-06 12:39:42 -0500

    Initial Commit

---
+++ sssss/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sssss
+//
+//  Created by Hanghang Zhang on 3/6/15.
+//  Copyright (c) 2015 Hanghang Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
